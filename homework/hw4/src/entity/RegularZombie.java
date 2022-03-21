638837a4cd22fa4f721b299e6f21863e838a775e
Max <kolxy@qq.com>
2022-03-21 01:09:38 -0700

    revamp composite implementation

--- homework/hw4/src/entity/RegularZombie.java
+++ homework/hw4/src/entity/RegularZombie.java
@@ -1,10 +1,25 @@
 package entity;
 
-public class RegularZombie extends Zombie {
+public class RegularZombie implements Zombie {
+    private int hp;
     public RegularZombie() {
         this.hp = 50;
     }
 
+    @Override
+    public Zombie takeDamage(int damage) {
+        hp -= damage;
+        if (hp <= 0)
+            return null;
+        return this;
+    }
+
+    @Override
+    public int getHp() {
+        return this.hp;
+    }
+
+    @Override
     public String toString() {
         return "R/" + this.getHp();
     }
--- homework/hw4/src/entity/Zombie.java
+++ homework/hw4/src/entity/Zombie.java
@@ -1,34 +1,7 @@
 package entity;
 
-public abstract class Zombie {
-    int hp;
-    Zombie zombie;
-
-    /**
-     * Get total hp of current zombie
-     * @return
-     */
-    public int getHp() {
-        if (zombie == null) {
-            return hp;
-        }
-        return hp + zombie.getHp();
-    }
-
-    /**
-     * Calculates hp after being damaged. Null if zombie is dead.
-     * @param damage
-     * @return damaged zombie
-     */
-    public Zombie takeDamage(int damage) {
-        hp -= damage;
-        if (hp <= 0) { // Composite dies
-            if (zombie == null) {
-                return null;
-            } else { // carry over damage
-                return zombie.takeDamage(0 - hp);
-            }
-        }
-        return this;
-    }
+public interface Zombie {
+    public Zombie takeDamage(int damage);
+    public int getHp();
+    public String toString();
 }
