4ffa5a2ac4a8cd2d912618b11b13ef643ae2b973
Max <kolxy@qq.com>
2022-03-21 01:31:52 -0700

    hw4 code comment

--- homework/hw4/src/entity/Zombie.java
+++ homework/hw4/src/entity/Zombie.java
@@ -1,7 +1,22 @@
 package entity;
 
 public interface Zombie {
+    /**
+     * Return the damaged Zombie, null if the zombie dies
+     * @param damage
+     * @return
+     */
     public Zombie takeDamage(int damage);
+
+    /**
+     * Calculates current hp
+     * @return
+     */
     public int getHp();
+
+    /**
+     * Format for print
+     * @return
+     */
     public String toString();
 }
